577b46bc040a3aff3242429d49a8367cf290c5b6
Shahidbilagi <shahidbilagi7@gmail.com>
2025-04-24 11:15:31 +0530

    save file

---
+++ math.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+
+        System.out.println("The first number is: " + num1);
+        System.out.println("The second number is: " + num2);
+        System.out.println("The sum is: " + sum);
+        System.out.println("The difference is: " + difference);
+    }
+}
